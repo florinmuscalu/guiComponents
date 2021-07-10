46615c11aa579acc8581c08da0d109a82922060b
florin.muscalu@gmail.com <florin.muscalu@gmail.com>
2021-07-10 10:56:40 +0300

    FractionButton improvement.

--- gui/src/main/java/tech/zekon/gui/FractionButton.java
+++ gui/src/main/java/tech/zekon/gui/FractionButton.java
@@ -5,18 +5,16 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.util.AttributeSet;
-import android.view.Gravity;
-import android.view.MotionEvent;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class FractionButton  extends ConstraintLayout implements View.OnClickListener {
-    private TextView mInteger, mNumerator, mDenominator;
+    private TextView mText, mNumerator, mDenominator;
     private boolean isCheckbox = false;
     private String Text, Numerator, Denominator;
     private boolean isChecked = false;
@@ -48,25 +46,15 @@ public class FractionButton  extends ConstraintLayout implements View.OnClickLis
 
     @SuppressLint("ResourceType")
     protected void prepareView(AttributeSet attrSet) {
-        mInteger = findViewWithTag(getResources().getString(R.string.tech_zekon_guiComponents_FractionButton_integer));
+        mText = findViewWithTag(getResources().getString(R.string.tech_zekon_guiComponents_FractionButton_integer));
         mNumerator = findViewWithTag(getResources().getString(R.string.tech_zekon_guiComponents_FractionButton_numerator));
         mDenominator = findViewWithTag(getResources().getString(R.string.tech_zekon_guiComponents_FractionButton_denominator));
 
-        if (mInteger == null || mNumerator == null || mDenominator == null) {
+        if (mText == null || mNumerator == null || mDenominator == null) {
             throw new IllegalStateException("Please provide a valid xml layout");
         }
 
-        int[] attrs = {
-                android.R.attr.textColor,   //0
-                android.R.attr.textSize,    //1
-        };
-
-        TypedArray ta = getContext().obtainStyledAttributes(attrSet, attrs);
-
-        int textColor = ta.getColor(0, Color.BLACK);
-        float textSize = ta.getFloat(1, 20);
-        ta.recycle();
-        ta = getContext().obtainStyledAttributes(attrSet, R.styleable.FractionButton);
+        TypedArray ta = getContext().obtainStyledAttributes(attrSet, R.styleable.FractionButton);
         isCheckbox = ta.getBoolean(R.styleable.FractionButton_isCheckbox, false);
         isChecked = ta.getBoolean(R.styleable.FractionButton_Checked, false);
         Text = ta.getString(R.styleable.FractionButton_Text);
@@ -75,15 +63,17 @@ public class FractionButton  extends ConstraintLayout implements View.OnClickLis
         if (Numerator == null) Numerator = "";
         Denominator = ta.getString(R.styleable.FractionButton_Denominator);
         if (Denominator == null) Denominator = "";
+        int textColor = ta.getColor(R.styleable.FractionButton_android_textColor, Color.BLACK);
+        float textSize = ta.getDimension(R.styleable.FractionButton_android_textSize, 20);
         ta.recycle();
 
-        mInteger.setTextColor(textColor);
+        mText.setTextColor(textColor);
         mNumerator.setTextColor(textColor);
         mDenominator.setTextColor(textColor);
 
-        mInteger.setTextSize(textSize);
-        mNumerator.setTextSize(textSize);
-        mDenominator.setTextSize(textSize);
+        mText.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        mNumerator.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        mDenominator.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
 
         setCheckbox(isCheckbox);
         setChecked(isChecked);
@@ -97,7 +87,7 @@ public class FractionButton  extends ConstraintLayout implements View.OnClickLis
     }
 
     public TextView getTextView() {
-        return mInteger;
+        return mText;
     }
 
     public TextView getNumeratorView() {
@@ -129,7 +119,7 @@ public class FractionButton  extends ConstraintLayout implements View.OnClickLis
 
     public void setText(String strInteger) {
         this.Text = strInteger;
-        mInteger.setText(strInteger);
+        mText.setText(strInteger);
     }
 
     public String getNumerator() {
